b554e0e458c81210f67eb667c47b5972a999ab69
Hari Gokul <ceeeharigokul23@gmail.com>
2023-05-10 15:46:15 +0530

    Hari

---
+++ First.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class First{
+int a=4;
+float b=23.3f;
+double c=a+b;
+public static void main(Stirng args[])
+{
+System.out.println("Hello Double ");
+First first=new First();
+first.First();
+}
+}
